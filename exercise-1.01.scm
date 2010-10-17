39520df0bda00a6eba5d34dcf0c6e3d6fb8965fc
Jason Gessner <jason@multiply.org>
2010-10-16 21:26:04 -0500

    Added exercise 1.1 file.

---
+++ exercise-1.01.scm
@@ -0,0 +1,35 @@
+#lang planet neil/sicp
+
+; Exercise 1.1 from SICP
+; The comment after each expression is the expected output.
+10
+; 10
+(+ 5 3 4)
+; 12 
+(- 9 1)
+; 8
+(/ 6 2)
+; 3
+(+ (* 2 4) (- 4 6))
+; 6  
+(define a 3)
+(define b (+ a 1))
+(+ a b (* a b))
+; 19
+(= a b)
+; #f
+(if (and (> b a) (< b (* a b)))
+    b
+    a)
+; 4
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+; 16
+(+ 2 (if (> b a) b a))
+; 6
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))
+; 16 
