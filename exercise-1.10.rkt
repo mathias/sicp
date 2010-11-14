dc497074619f07c5f2d20d595f51674c2a1e63ac
Matt Gauger <matt.gauger@gmail.com>
2010-11-14 01:59:28 -0600

    finish up Exercise 1.10

---
+++ exercise-1.10.rkt
@@ -0,0 +1,63 @@
+#lang planet neil/sicp
+
+; SICP Exercise 1.10
+
+; The following procedure computes a mathematical function called Ackermann's function.
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+; What are the values of the following expressions? 
+(A 1 10)
+; (A 0 (A 1 9))
+; (A 0 (A 0 (A 1 8)))
+; (A 0 (A 0 (A 0 (A 1 7))))
+; (A 0 (A 0 (A 0 (A 0 (A 1 6)))))
+; (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 5))))))
+; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 4)))))))
+; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 3))))))))
+; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 2)))))))))
+; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 1))))))))))
+; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2)))))))))
+; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))
+; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 8)))))))
+; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16))))))
+; (A 0 (A 0 (A 0 (A 0 (A 0 32)))))
+; (A 0 (A 0 (A 0 (A 0 64))))
+; (A 0 (A 0 (A 0 128)))
+; (A 0 (A 0 256))
+; (A 0 512)
+; 1024
+
+(A 2 4)
+; (A 1 (A 2 3))
+; (A 1 (A 1 (A 2 2)))
+; (A 1 (A 1 (A 1 (A 2 1))))
+; (A 1 (A 1 (A 1 2)))
+; (A 1 (A 1 (A 0 (A 1 1))))
+; .. continues expanding based on condition in function above
+; Answer: 65536
+
+(A 3 3)
+; Answer: 65536 
+
+; Consider the following procedures, where A is the procedure defined above:
+(define (f n) (A 0 n))
+; computes 2n
+
+(define (g n) (A 1 n))
+; computes 2^n
+; example: (A 1 10) = 2^10 = 1024
+
+(define (h n) (A 2 n))
+; computes 2^(h (n-1)) 
+; example (h 1) = 2^(h 0) = 2^0 = 2
+; example (h 2) = 2^(h 1) = 2^2 = 4
+; example (h 3) = 2^(h 2) = 2^4 = 16
+
+(define (k n) (* 5 n n))
+; computes 5 * n^2
